ba7d43a65f56255caaa67610eda6d32b834a1fd2
albusiness98 <72590054+albusiness98@users.noreply.github.com>
2021-02-10 17:06:26 -0600

    Update hipotenusa.php

--- hipotenusa.php
+++ hipotenusa.php
@@ -1,14 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>CALCULO DE LA HIPOTENUSA DEL TRIANGULO RECTANGULO</title>
+</head>
+<body>
 <?php
 
-$cateto1 = 3;
-$cateto2 = 4;
-$hipotenusa = null;
-
-$hipotenusa = sqrt(pow($cateto1, 2) + pow($cateto2, 2));
-
-echo 'Calcular la hipotenusa del triangulo rectangulo', '<br/>';
-echo 'Cateto1:'. $cateto1, '<br/>';
-echo 'Cateto2:'. $cateto2, '<br/>';
-echo 'Hipotenusa'. $hipotenusa, '<br/>';
-
-?>
+  if(isset($_POST["button"])&& $_POST["btn"] == 'Calcular') {
+  
+    $cateto1 = $_POST['cateto 1'];
+    $cateto2 = $_POST['cateto 2'];    
+    $hipotenusa = null;
+    
+   if(!empty($cateto1) or !empty($cateto2)) {
+   
+    echo "<h2 align='center'>CALCULO DE LA HIPOTENUSA DEL TRIANGULO RECTANGULO</h2>";
+    
+      $hipotenusa = sqrt(pow($cateto1, 2) + pow($cateto2, 2));
+      
+    echo "<br>Cateto 1: ". $cateto1;
+    echo "<br>Cateto 2: ". $cateto2;   
+    echo "<br/><br/>";
+    echo "<br>Hipotenusa: ". $hipotenusa;
+    echo "<br/><br/>";
+    echo "<a href='datos.html'>Regresar</a>";
+   }
+  }
+ ?>
+ </body>
+</html>
