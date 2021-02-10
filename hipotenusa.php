8059eb48c12afaa28538ad032c480d1955f2bedb
albusiness98 <72590054+albusiness98@users.noreply.github.com>
2021-02-10 15:17:57 -0600

    Create hipotenusa.php

---
+++ hipotenusa.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head><title>CALCULO DE LA HIPOTENUSA DEL TRIANGULO RECTANGULO</title><head>
+<body>
+<?php
+
+  if(isset($_POST["button"])&& $_POST["button"] == 'Calcular'){
+  
+    $cateto1 = $_POST['cateto 1'];
+    $cateto2 = $_POST['cateto 2'];    
+    $hipotenusa = null;
+    
+   if(!empty($cateto1) or !empty($cateto2)) {
+   
+    echo "<h2 align='center'>CALCULO DE LA HIPOTENUSA DEL TRIANGULO RECTANGULO</h2>";
+    
+      $hipotenusa = sqrt(pow($cateto1, 2) + pow($cateto2, 2));
+      
+    echo "<br>Cateto 1: ". $cateto1;
+    echo "<br>Cateto 2: ". $cateto2;   
+    echo "<br/><br/>";
+    echo "<br>Hipotenusa: ". $hipotenusa;
+    echo "<br/><br/>";
+    echo "<a href='datos.html'>Regresar</a>";
+   }
+  }
+ ?>
+ </body>
+</html>
